c226340e4701b2f8015bd799322b194374d13986
alsergeevich <alsergeevich@gmail.com>
2024-02-23 00:07:52 +0500

    Добавил ДЗ1 задания 1,2,3

---
+++ DZ1_Base_C/Task2/task2.c
@@ -0,0 +1,14 @@
+/*Вывести на экран текст "лесенкой"
+	 Let’s
+       go
+		to walk
+*/
+
+#include <stdio.h>
+
+int main(void) {
+
+    printf("     %s\n", "Let`s");
+    printf("       %s\n", "go");
+    printf("        %s\n", "to walk");
+}
---
+++ DZ1_Base_C/Task3/task3.c
@@ -0,0 +1,21 @@
+/*Вывести на экран рисунок из букв
+     *
+    ***
+   *****
+  *******
+   HH HH
+   ZZZZZ
+*/
+
+#include <stdio.h>
+
+int main(void) {
+
+    printf("     %s\n", "*");
+    printf("    %s\n", "***");
+    printf("   %s\n", "*****");
+    printf("  %s\n", "*******");
+    printf("   %s\n", "HH HH");
+    printf("   %s\n", "ZZZZZ");
+
+}
